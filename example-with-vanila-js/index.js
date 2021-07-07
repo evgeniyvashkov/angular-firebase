4bb5d56984919e6535f9b6500e6e589a8bc62e7d
LEVERX\Yauheni.Vashkou <yauheni.vashkou@leverx.com>
2021-07-07 14:01:51 +0300

    vanila guide

--- example-with-vanila-js/index.js
+++ example-with-vanila-js/index.js
@@ -7,68 +7,164 @@ const firebaseConfig = {
   appId: "1:458568985532:web:e5025c244cfbf465b359b9"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+const ACTIONS_TYPE = {
+  EDIT: 1,
+  DELETE: 2
+}
 
-const addBtn = document.getElementById('add');
-const postField = document.getElementById('post-field');
-const postsList = document.getElementById('posts-list');
-const signInBtn = document.getElementById('sign-in');
-const signOutBtn = document.getElementById('sign-out');
 const provider = new firebase.auth.GoogleAuthProvider();
-
+const app = document.getElementById('app');
 const firestore = firebase.firestore();
 const postsRef = firestore.collection('posts');
+let signInBtn;
+let signOutBtn;
+let posts = [];
+let editablePost;
+let addBtn;
+let postField;
+let postsList;
+let logged = false;
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    initApp(user);
+  } else {
+    app.innerHTML = '<button id="sign-in">Sign In</button>';
+    signInBtn = document.getElementById('sign-in');
+    signInBtn.addEventListener('click', signIn);
+  }
+})
 
 function getPosts() {
   postsRef.get().then(response => {
-      const posts = response.docs.map(_post => _post.data());
-      console.log(posts);
-      let postsLayout = '';
+    posts = response.docs.map(post => ({ id: post.id, ...post.data() }));
+    console.log(posts);
+    let postsLayout = '';
 
-      posts.forEach(post => {
-          postsLayout += `
-              <li>${post.text}</li>
-              <button id="edit">Edit</button>
-              <button id="delete">Delete</button>`
+    posts.forEach(post => {
+      postsLayout += `
+              <li data-post-id="${post.id}">
+                <span>${post.text}</span>
+                <button data-action-type="${ACTIONS_TYPE.EDIT}">Edit</button>
+                <button data-action-type="${ACTIONS_TYPE.DELETE}">Delete</button>
+              </li>`
+    })
 
-      })
-
-      postsList.innerHTML = postsLayout;
+    postsList.innerHTML = postsLayout;
   })
 }
 
 function addPost() {
+  if (!postField.value) {
+    return;
+  }
+
   const post = { text: postField.value };
 
   postsRef.add(post).then(() => {
-      console.log('post added');
-      getPosts();
-      postField.value = '';
+    console.log('post added');
+    getPosts();
+    postField.value = '';
   })
 }
 
-addBtn.addEventListener('click', addPost);
+function deletePost(id) {
+  postsRef.doc(id).delete()
+    .then(() => {
+      alert('Post deleted');
+      getPosts();
+    })
+    .catch(() => console.log('Oops...'))
+}
 
-signInBtn.addEventListener('click', signIn)
-signOutBtn.addEventListener('click', signOut);
+function editPost(post) {
+  addBtn.removeEventListener('click', addPost);
+  postField.value = post.text;
+  addBtn.innerText = 'Update';
+  editablePost = post;
+  addBtn.addEventListener('click', updatePost);
+}
+
+function updatePost() {
+  postsRef.doc(editablePost.id).set({
+    text: postField.value
+  })
+    .then(() => {
+      setAddMode();
+      getPosts();
+    })
+    .catch(() => console.log('Oops...'))
+
+}
 
 function signIn() {
-  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
-      .then(() => {
-          return firebase.auth().signInWithPopup(provider)
-      })
-      .then(res => {
-          console.log(`You have been signed on as ${res.user.displayName}`)
-      }).catch(err => {
+  firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      console.log(`You have been signed on as ${res.user.displayName}`)
+      initApp();
+    }).catch(err => {
       console.log('Oops ...')
-  });
+    });
 }
 
 function signOut() {
-  firebase.auth().signOut().then(() => console.log('You have been signed out'))
+  firebase.auth().signOut().then(() => {
+    console.log('You have been signed out');
+    app.innerHTML = '<button id="sign-out">Sign Out</button>';
+  })
 }
 
-getPosts();
+function setAddMode() {
+  addBtn.removeEventListener('click', updatePost);
+  addBtn.innerText = 'Add';
+  postField.value = '';
+  editablePost = null;
+  addBtn.addEventListener('click', addPost);
+}
 
+function setEditMode(post) {
+  addBtn.removeEventListener('click', addPost);
+  postField.value = post.text;
+  addBtn.innerText = 'Update';
+  editablePost = post;
+  addBtn.addEventListener('click', updatePost);
+}
+
+function initApp(user) {
+  app.innerHTML = `
+      <h2>${user ? user.displayName : null}</h2>
+      <button id="sign-out">Sign Out</button>
+
+      <div>
+        <textarea id="post-field"></textarea>
+        <button id="add">Add</button>
+      </div>
+
+      <ul id="posts-list"></ul>
+  `
+  addBtn = document.getElementById('add');
+  postField = document.getElementById('post-field');
+  postsList = document.getElementById('posts-list');
+  signOutBtn = document.getElementById('sign-out');
+  signOutBtn.addEventListener('click', signOut);
+
+  addBtn.addEventListener('click', addPost);
+
+  postsList.addEventListener('click', ({ target }) => {
+    const actionType = Number(target.dataset.actionType);
+    const postId = target.parentElement.dataset.postId;
+
+    if (actionType === ACTIONS_TYPE.EDIT) {
+      const post = posts.find(post => post.id === postId);
+      editPost(post);
+    }
+
+    if (actionType === ACTIONS_TYPE.DELETE) {
+      deletePost(postId);
+    }
+  })
+
+  getPosts();
+}
---
+++ example-with-vanila-js/readme.md
@@ -0,0 +1,116 @@
+1. Creating Firebase Project 
+  + login to the [firebase](https://firebase.google.com/) 
+  + open [console](https://console.firebase.google.com/)  and click "Add project" button
+  + enter project name (firebase-angular-tutorial) => continue
+  + optional you can enable/disable Google Analytics (I prefer disable) => create project and wait and click => continue
+  + now you located on firebase console, navigate to left sidebar and chose authentication tab => click get started => Sign-in method tab => find Google option  => enable it (enter your email) => save
+  + look at left sidebar menu and and click 'Firestore Database' => create database => Start in test mode => choose cloud firestore location (eur3) => enable
+  + Chose Project overview (Under Firebase logo) and add Web app => enter name (web-app) => register app => continue to console
+  + navigate to Project overview and click settings icon and choose Project Settings 
+  
+  + on General tab scroll down and find your app (in my case web-app), set Config in SDK setup and configuration and copy firebaseConfig to your local project/environments/environment.ts
+  
+2. Setup project:
+  + Include scripts:
+  **Warning: make sure that you project is running in localhost:port number** (webstorm starts automatically, if VS Code use Live Server extension)
+  ```js
+    <script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-auth.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-firestore.js"></script>
+  ```
+
+  + Init app with configuration:
+    ```js
+      const firebaseConfig = {
+        apiKey: "AIzaSyBWU-epHDG3P4-2pwUW6Z3suiTLBNHcKlQ",
+        authDomain: "my-project-abd93.firebaseapp.com",
+        projectId: "my-project-abd93",
+        storageBucket: "my-project-abd93.appspot.com",
+        messagingSenderId: "458568985532",
+        appId: "1:458568985532:web:e5025c244cfbf465b359b9"
+      };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    ```
+3. Authentication setup:
+  + **Sign In**:
+  ```js
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    function signIn() {
+      firebase.auth().signInWithPopup(provider)
+        .then(res => {
+          console.log(`You have been signed on as ${res.user.displayName}`)
+          initApp();
+        }).catch(err => {
+          console.log('Oops ...')
+        });
+    }
+  ```
+  
+  + **Sign out**:
+  ```js
+    function signOut() {
+      firebase.auth().signOut().then(() => {
+      ...
+    })  
+  }
+  ```
+
+  + **Keep User Logged**:
+  ```js
+    firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      ...
+    } else {
+      ...
+    }
+  })
+  ```
+
+4. CRUD operations  with Firestore:
+  + **Get**:
+  ```ts
+      function getData() {
+        firestore.collection('posts')
+          .get()
+          .then(response => {
+            const posts = response.docs.map(post => ({id: post.id, ...post.data()}));
+            ...
+      }
+  ```
+
+  + **Add**:
+  ```js
+    function addPost(post) {
+      firestore.collection('posts')
+        .add(post)
+        .then(() => {
+          ...
+        })
+    }
+
+  ```
+
+  + **Update** 
+  ```js
+    function updatePost(postId) {
+      firestore.collection('posts')
+        .doc(postId)
+        .set({
+          text: postField.value
+        })
+        .then( ... )
+    }
+  ```
+
+  + **Delete**
+  ```js
+    function deletePost(postId) {
+      firestore.collection('posts')
+        .doc(id)
+        .delete()
+        .then( ... )
+    }
+  ```
